b8e668d5342584ae1ff086beb2a3a0be95c72fd1
parrycj <parrycj@vcu.edu>
2017-03-25 23:13:02 -0400

    Added button for Paul

--- app/src/main/java/com/jzheadley/androidvideosearch/MainActivity.java
+++ app/src/main/java/com/jzheadley/androidvideosearch/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jzheadley.androidvideosearch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Paul's Button
+        Button PaulsButton = (Button) findViewById(R.id.PaulsButton);
+        PaulsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //insert code here
+            }
+        });
+
+        //Record Button
+        Button recordButton = (Button) findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //insert code here
+            }
+        });
+
+
+        //Gallery Button
+        Button galleryButton = (Button) findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //insert code here
+            }
+        });
+
     }
 }
